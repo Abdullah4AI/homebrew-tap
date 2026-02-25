b732499743d89efc66a51f404364cc6ee78ae63b
Abdullah AlRashoudi <abdullahalrashoudi@Abdullah4AIs-Mac-mini.local>
2026-02-25 16:07:40 +0300

    bump: appledev v1.260225.3

--- Formula/appledev.rb
+++ Formula/appledev.rb
@@ -1,8 +1,8 @@
 class Appledev < Formula
   desc "Apple Developer Toolkit - unified CLI for App Store Connect + iOS app builder"
   homepage "https://github.com/Abdullah4AI/apple-developer-toolkit"
-  url "https://github.com/Abdullah4AI/apple-developer-toolkit/archive/refs/tags/v1.260225.0.tar.gz"
-  sha256 "d30499efb3ec218e578d1208ea53804c2066e2e9f2363f1693065989058a13dc"
+  url "https://github.com/Abdullah4AI/apple-developer-toolkit/archive/refs/tags/v1.260225.3.tar.gz"
+  sha256 "73b4b57a4bfa1a7716284b7acc58f945105d24bcc54a118718314ee3689d5d0f"
   license "MIT"
 
   depends_on "go" => :build
